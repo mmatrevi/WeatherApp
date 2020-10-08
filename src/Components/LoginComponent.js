94c3155b292ed01f60cdf872fa369323b1caa820
mmatrevi <43908690+mmatrevi@users.noreply.github.com>
2020-10-08 07:57:41 +0000

    Update LoginComponent.js

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -11,7 +11,7 @@ function Loginform() {
        <div className='Loginform'>
           <p style={{fontSize:'40px', paddingLeft:'49px'}}>The Weather App</p>
             <p><input style={{margin:'20px',width:'350px', height:'30px'}} type='text' name='' placeholder='Enter your username'></input></p>
-             <button style={{fontSize:'30px', marginLeft:'150px'}} onClick={SubmitButton()}>Login in</button>
+             <button style={{fontSize:'30px', marginLeft:'150px'}} onClick={SubmitButton}>Login in</button>
        </div>
     );
 }
